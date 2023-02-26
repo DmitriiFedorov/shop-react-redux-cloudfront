0ea6ec3bbba7aea30ba866e64a3652a476f34000
Dmitry Fedorov <dmitry_fedorov1@epam.com>
2023-02-26 13:33:55 +0300

    feat: update bff API_PATH

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://x2qhp74na8.execute-api.eu-west-1.amazonaws.com",
+  bff: "https://x0yn0qc56e.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
