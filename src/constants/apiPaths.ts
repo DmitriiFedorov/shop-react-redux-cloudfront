5641d2a3d6094f72565fcb1731be89847be33c29
Dmitry Fedorov <dmitry_fedorov1@epam.com>
2023-04-16 13:44:59 +0300

    feat: apdate apiPath for the cart service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://4uf9uh9uq4.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://x0yn0qc56e.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "http://cart-api-app-v3-dev.eu-west-1.elasticbeanstalk.com/",
 };
 
 export default API_PATHS;
